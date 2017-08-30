2282389f59b8595befbbc9ebda0e62fe3ee8c01d
ben <benying1988@gmail.com>
2017-08-30 17:15:21 +0800

    update scrollview changed listener

--- RxJava/app/src/main/java/com/yjh/rxjava/MainActivity.java
+++ RxJava/app/src/main/java/com/yjh/rxjava/MainActivity.java
@@ -27,7 +27,6 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
